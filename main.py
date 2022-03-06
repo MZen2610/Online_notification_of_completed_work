ec5062f39b21f71026513fad3fbdc8e0f4c28eb0
MZen2610 <mzen1980@gmail.com>
2022-03-06 21:50:02 +0300

    подправил текст

--- main.py
+++ main.py
@@ -31,19 +31,18 @@ def send_request():
     dvmn_token = os.environ["DVMN_TOKEN"]
     tgm_token = os.environ["TGM_TOKEN"]
     chat_id = os.environ["CHAT_ID"]
-    text_bot = ""
 
     result_checking = verified_works(dvmn_token)
     if result_checking["new_attempts"][0]["is_negative"] == True:
         text = f'К сожалению, в работе нашлись ошибки.\n\n' \
-                   f'ссылка на урок {result_checking["new_attempts"][0]["lesson_url"]}'
+               f'ссылка на урок {result_checking["new_attempts"][0]["lesson_url"]}'
     else:
         text = "Преподавателю всё понравилось, можно приступать к следующему уроку."
     text_bot = f'У вас проверили работу "{result_checking["new_attempts"][0]["lesson_title"]}"\n\n {text}'
 
-
     bot = telegram.Bot(token=tgm_token)
     bot.send_message(chat_id=chat_id, text=text_bot)
+
 
 if __name__ == '__main__':
     send_request()
