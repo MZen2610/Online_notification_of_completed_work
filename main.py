ca4bae378f1a50d2ea97acff7d10d0810e331a31
MZen2610 <mzen1980@gmail.com>
2022-03-08 17:44:03 +0300

    переименовал переменную

--- main.py
+++ main.py
@@ -33,13 +33,13 @@ def main():
     tgm_token = os.environ["TGM_TOKEN"]
     chat_id = os.environ["TGM_CHAT_ID"]
 
-    result_checking = get_verified_works(dvmn_token)
-    if result_checking["new_attempts"][0]["is_negative"] == True:
+    checking_result = get_verified_works(dvmn_token)
+    if checking_result["new_attempts"][0]["is_negative"] == True:
         text = f'К сожалению, в работе нашлись ошибки.\n\n' \
-               f'ссылка на урок {result_checking["new_attempts"][0]["lesson_url"]}'
+               f'ссылка на урок {checking_result["new_attempts"][0]["lesson_url"]}'
     else:
         text = "Преподавателю всё понравилось, можно приступать к следующему уроку."
-    text_bot = f'У вас проверили работу "{result_checking["new_attempts"][0]["lesson_title"]}"\n\n {text}'
+    text_bot = f'У вас проверили работу "{checking_result["new_attempts"][0]["lesson_title"]}"\n\n {text}'
 
     bot = telegram.Bot(token=tgm_token)
     bot.send_message(chat_id=chat_id, text=text_bot)
