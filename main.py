7643206a31929d34f1db7478e9119fcdd46339f5
MZen2610 <mzen1980@gmail.com>
2022-03-08 19:03:01 +0300

    переписал логику отправки сообщений

--- main.py
+++ main.py
@@ -5,6 +5,23 @@ import requests
 import telegram
 import time
 
+load_dotenv()
+dvmn_token = os.environ["DVMN_TOKEN"]
+tgm_token = os.environ["TGM_TOKEN"]
+chat_id = os.environ["TGM_CHAT_ID"]
+
+
+def send_telegram_message(checking_result):
+    if checking_result["new_attempts"][0]["is_negative"] == True:
+        text = f'К сожалению, в работе нашлись ошибки.\n\n' \
+               f'ссылка на урок {checking_result["new_attempts"][0]["lesson_url"]}'
+    else:
+        text = "Преподавателю всё понравилось, можно приступать к следующему уроку."
+    text_bot = f'У вас проверили работу "{checking_result["new_attempts"][0]["lesson_title"]}"\n\n {text}'
+
+    bot = telegram.Bot(token=tgm_token)
+    bot.send_message(chat_id=chat_id, text=text_bot)
+
 
 def get_verified_works(token):
     headers = {"Authorization": token}
@@ -17,11 +34,10 @@ def get_verified_works(token):
                                     timeout=10
                                     )
             response.raise_for_status()
-            response_json = response.json()
-            if response_json["status"] == "timeout":
-                params = {"timestamp": response_json["timestamp_to_request"]}
-            return response_json
-
+            result = response.json()
+            if result["status"] == "timeout":
+                params = {"timestamp": result["timestamp_to_request"]}
+            send_telegram_message(result)
         except requests.exceptions.ReadTimeout:
             continue
         except ConnectionError:
@@ -29,21 +45,7 @@ def get_verified_works(token):
 
 
 def main():
-    load_dotenv()
-    dvmn_token = os.environ["DVMN_TOKEN"]
-    tgm_token = os.environ["TGM_TOKEN"]
-    chat_id = os.environ["TGM_CHAT_ID"]
-
-    checking_result = get_verified_works(dvmn_token)
-    if checking_result["new_attempts"][0]["is_negative"] == True:
-        text = f'К сожалению, в работе нашлись ошибки.\n\n' \
-               f'ссылка на урок {checking_result["new_attempts"][0]["lesson_url"]}'
-    else:
-        text = "Преподавателю всё понравилось, можно приступать к следующему уроку."
-    text_bot = f'У вас проверили работу "{checking_result["new_attempts"][0]["lesson_title"]}"\n\n {text}'
-
-    bot = telegram.Bot(token=tgm_token)
-    bot.send_message(chat_id=chat_id, text=text_bot)
+    get_verified_works(dvmn_token)
 
 
 if __name__ == '__main__':
