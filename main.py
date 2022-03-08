4885d506798d8c557431011853c57341575dd2c2
MZen2610 <mzen1980@gmail.com>
2022-03-08 20:21:10 +0300

    очистил код от индексов

--- main.py
+++ main.py
@@ -12,12 +12,22 @@ chat_id = os.environ["TGM_CHAT_ID"]
 
 
 def send_telegram_message(checking_result):
-    if checking_result["new_attempts"][0]["is_negative"]:
-        text = f'К сожалению, в работе нашлись ошибки.\n\n' \
-               f'ссылка на урок {checking_result["new_attempts"][0]["lesson_url"]}'
-    else:
-        text = "Преподавателю всё понравилось, можно приступать к следующему уроку."
-    text_bot = f'У вас проверили работу "{checking_result["new_attempts"][0]["lesson_title"]}"\n\n {text}'
+    text = ""
+    lesson_title = ""
+    for key_result, value_result in checking_result.items():
+        if key_result == "new_attempts":
+            for values_new_attempt in value_result:
+                for key_new_attempt, value_new_attempt in values_new_attempt.items():
+                    if key_new_attempt == "is_negative" and value_new_attempt:
+                        text = f'К сожалению, в работе нашлись ошибки.\n\n'
+                    elif key_new_attempt == "is_negative" and not value_new_attempt:
+                        text = "Преподавателю всё понравилось, можно приступать к следующему уроку."
+                    elif key_new_attempt == "lesson_url":
+                        text += f'ссылка на урок {value_new_attempt}'
+                    elif key_new_attempt == "lesson_title":
+                        lesson_title = value_new_attempt
+
+    text_bot = f'У вас проверили работу "{lesson_title}"\n\n {text}'
 
     bot = telegram.Bot(token=tgm_token)
     bot.send_message(chat_id=chat_id, text=text_bot)
