036b7d4bce5b6364b7a710fec9ca4ab00baeca9e
MZen2610 <mzen1980@gmail.com>
2022-03-15 20:38:20 +0300

    send logs to Telegram to admin user

--- main.py
+++ main.py
@@ -6,6 +6,16 @@ import telegram
 import time
 import logging
 
+class TelegramLogsHandler(logging.Handler):
+
+    def __init__(self, tg_bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = tg_bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
 
 def send_telegram_message(checking_result, tgm_token, chat_id):
     last_attempt = checking_result['new_attempts'][0]
@@ -48,9 +58,11 @@ def main():
     dvmn_token = os.environ["DVMN_TOKEN"]
     tgm_token = os.environ["TGM_TOKEN"]
     chat_id = os.environ["TGM_CHAT_ID"]
-    logging.warning("Бот запущен")
+    logger = logging.getLogger("Logger")
+    logger.setLevel(logging.WARNING)
+    logger.addHandler(TelegramLogsHandler(tgm_token, chat_id))
     check_work(dvmn_token, tgm_token, chat_id)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
